6fd5aa462290e93a94d8bfa1f9a6f68a9c9b930f
noborder <noborder_d.w@foxmail.com>
2018-01-12 18:38:26 +0800

    1:add copy paste function,can copy and paste across different process 2:More universally,but more slow

--- ByamlEdit/ShowNode.cs
+++ ByamlEdit/ShowNode.cs
@@ -15,5 +15,38 @@ namespace ByamlEdit
         {
             
         }
+
+        public ShowNode(JsonClip jsonClip) : base(jsonClip.Text)
+        {
+            Type = jsonClip.Type;
+          
+            foreach(var item in jsonClip.childs)
+            {
+                Nodes.Add(new ShowNode(item));
+            }
+        }
+    }
+
+    class JsonClip
+    {
+        public ByamlNodeType Type;
+        public string Text;
+        public List<JsonClip> childs;
+
+        public JsonClip(ShowNode sNode)
+        {
+            childs = new List<JsonClip>();
+            Type = sNode.Type;
+            Text =sNode.Text;
+            foreach (var node in sNode.Nodes)
+            {
+                childs.Add(new JsonClip((ShowNode)node));
+            }
+        }
+
+        public JsonClip()
+        {
+            childs = new List<JsonClip>();
+        }
     }
 }
